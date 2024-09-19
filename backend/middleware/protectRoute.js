4da04906351b6b61a070b345b67a12f41ed2704d
Vi Nguyen <levinguyen170602@gmail.com>
2024-09-19 09:44:47 +0700

    fix protectRoute

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -7,7 +7,9 @@ const protectRoute = async (req, res, next) => {
     if (!token) {
       const authHeader = req.headers.authorization;
       if (!authHeader || !authHeader.startsWith("Bearer")) {
-        throw new UnauthenticatedError("Authentication invalid");
+        return res
+          .status(401)
+          .json({ error: "Unauthorized - No Token Provided" });
       }
       token = authHeader.split(" ")[1];
     }
