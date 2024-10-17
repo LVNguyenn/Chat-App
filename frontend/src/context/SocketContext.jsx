37474ff92654a5b56d10087fde12ee83dfaed919
Vi Nguyen <levinguyen170602@gmail.com>
2024-10-17 09:25:10 +0700

    update socket client

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("https://chat-app-mbd7.onrender.com", {
+            const socket = io("https://chat-app-l355.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 }
